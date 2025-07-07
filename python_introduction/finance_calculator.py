acdf2a95370022cc812820b8e4d503006d621b76
Okpunor Emmanuel <okpunorrex@gmail.com>
2025-07-07 19:43:54 +0300

    Refactor variable names for clarity in finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,9 @@
-income = int(input("Enter your monthly income: "))
-expenses = int(input("Enter your total monthly expenses: "))
-monthly_savings = income - expenses
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
 projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 
 print(f"Your monthly savings are ${monthly_savings}.")
 print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
+
+# monthly_savings = (monthly_income - monthly_expenses|float(monthly_income) - float(monthly_expenses))
