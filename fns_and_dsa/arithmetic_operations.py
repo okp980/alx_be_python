46f05ffe34fa0e79044afde3afb91914330014b5
Okpunor Emmanuel <okpunorrex@gmail.com>
2025-08-23 17:02:19 +0300

    Fix division operation in perform_operation function to handle zero numerator case correctly

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,6 +9,9 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return "Error: Division by zero"
-            return num1 / num2
+            elif num1 == 0:
+                return "Error: Division by zero"
+            else:
+                return num1 / num2
         case _:
             return "Invalid operation"
